688b34dd69999f5d7b1d82d73f580a6ec7b07407
Thomas Kishel <tom_kishel@ziba.com>
2016-10-13 16:54:51 -0700

    Discover environment from script path, pass to simplified ruby scripts.

--- scripts/config_version.sh
+++ scripts/config_version.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
-if [ -e $1/$2/.r10k-deploy.json ]
+
+pushd `dirname $0` > /dev/null
+SCRIPTPATH=`pwd`
+popd > /dev/null
+PE_ENVIRONMENT_PATH="$(dirname -- "$SCRIPTPATH")"
+#PE_ENVIRONMENTS_PATH="$(dirname -- "$PE_ENVIRONMENT_PATH")"
+#PE_ENVIRONMENT=$(basename -- "$PE_ENVIRONMENT_PATH")
+
+if [ -e $PE_ENVIRONMENT_PATH/.r10k-deploy.json ]
 then
-  /opt/puppetlabs/puppet/bin/ruby $1/$2/scripts/code_manager_config_version.rb $1 $2
+  /opt/puppetlabs/puppet/bin/ruby $PE_ENVIRONMENT_PATH/scripts/code_manager_config_version.rb $PE_ENVIRONMENT_PATH
 elif [ -e /opt/puppetlabs/server/pe_version ]
-then 
-  /opt/puppetlabs/puppet/bin/ruby $1/$2/scripts/config_version.rb $1 $2  
+then
+  /opt/puppetlabs/puppet/bin/ruby $PE_ENVIRONMENT_PATH/scripts/config_version.rb $PE_ENVIRONMENT_PATH
 else
   /usr/bin/git --version > /dev/null 2>&1 &&
-  /usr/bin/git --git-dir $1/$2/.git rev-parse HEAD ||
+  /usr/bin/git --git-dir PE_ENVIRONMENT_PATH/.git rev-parse HEAD ||
   date +%s
-fi 
+fi
