9d665786570d2991dc89e70b7f53cdd64048f464
Thomas Kishel <tom_kishel@ziba.com>
2016-10-13 16:56:32 -0700

    Use single parameter.

--- scripts/code_manager_config_version.rb
+++ scripts/code_manager_config_version.rb
@@ -1,9 +1,8 @@
 require 'json'
 
-environmentpath = ARGV[0]
-environment     = ARGV[1]
+puppet_environment_path = ARGV[0]
 
-r10k_deploy_file_path = File.join(environmentpath, environment, '.r10k-deploy.json')
+r10k_deploy_file_path = File.join(puppet_environment_path, '.r10k-deploy.json')
 
 #output the sha1 from the control-repo
 puts JSON.parse(File.read(r10k_deploy_file_path))['signature']
