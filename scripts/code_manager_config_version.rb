b69abc129fe7ba87145f713f1efe913ba9e9c649
Thomas Kishel <tom_kishel@ziba.com>
2016-10-14 11:25:29 -0700

    Merge into single script.

--- scripts/code_manager_config_version.rb
+++
@@ -1,8 +0,0 @@
-require 'json'
-
-puppet_environment_path = ARGV[0]
-
-r10k_deploy_file_path = File.join(puppet_environment_path, '.r10k-deploy.json')
-
-#output the sha1 from the control-repo
-puts JSON.parse(File.read(r10k_deploy_file_path))['signature']
