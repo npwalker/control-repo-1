b6fc2863a9a31845d5e501d2b92b42f6ec84d496
Thomas Kishel <tom_kishel@ziba.com>
2016-10-14 11:25:09 -0700

    Merge into single script.

--- scripts/config_version.rb
+++ scripts/config_version.rb
@@ -1,22 +1,41 @@
-begin
-  require 'rugged'
-rescue LoadError => e
-  t = Time.new
-  puts t.to_i
+
+script_path = File.expand_path(File.dirname(__FILE__))
+puppet_environment_path = File.dirname(script_path)
+
+environment_r10k_deploy_file = File.join(puppet_environment_path, '.r10k-deploy.json')
+pe_version_file = '/opt/puppetlabs/server/pe_version'
+environment_git_dir = File.join(puppet_environment_path, '.git')
+
+time_now_in_seconds = Time.now.to_i
+
+if (File.file?(environment_r10k_deploy_file))
+	begin
+	  require 'json'
+	rescue LoadError => e
+	  puts time_now_in_seconds
+	else
+		signature = JSON.parse(File.read(environment_r10k_deploy_file))['signature']
+		puts signature
+	end
+elsif (File.file?(pe_version_file))
+	begin
+	  require 'rugged'
+	rescue LoadError => e
+	  puts time_now_in_seconds
+	else
+	  repo = Rugged::Repository.discover(puppet_environment_path)
+	  repo_head  = repo.head
+	  repo_head_sha = head.target_id
+	  repo_head_commit_message = repo.lookup(head_sha)
+	  # (add something to find the remote url)
+	  puts repo_head_sha
+	end
 else
-
-  puppet_environment_path = ARGV[0]
-
-  repo = Rugged::Repository.discover(puppet_environment_path)
-  head  = repo.head
-
-  #sha1 hash of the newest commit
-  head_sha = head.target_id
-
-  #the commit message associated the newest commit
-  commit = repo.lookup(head_sha)
-
-  #add something to find the remote url
-
-  puts head_sha 
+	git_head_sha = %x( /usr/bin/git --git-dir #{environment_git_dir} rev-parse HEAD > /dev/null 2>&1 )
+	e = $?.exitstatus
+	if (e == 0)
+		puts git_head_sha
+	else
+		puts time_now_in_seconds
+	end
 end
